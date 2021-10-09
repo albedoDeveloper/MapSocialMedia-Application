cadcd3a29d0dc0122b194f9a0bd61d9571a7033c
Kye <50266459+albedoDeveloper@users.noreply.github.com>
2021-10-09 23:50:34 +0800

    Added in basic base phone app and incorporated map

---
+++ Social Media Map Application/Social Media Map Application/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Mapsui;
+
+
+namespace Social_Media_Map_Application
+{
+    public partial class MainPage : ContentPage
+    {
+        Map map = new Map
+        {
+            CRS = "EPSG:3857",
+        };
+
+        //to be removed
+        int testCounter = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            var tileLayer = Mapsui.Utilities.OpenStreetMap.CreateTileLayer();
+
+
+            map.Layers.Add(tileLayer);
+            //map.Widgets.Add(new Mapsui.Widgets.ScaleBar.ScaleBarWidget(map) { TextAlignment = Mapsui.Widgets.Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Left, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Bottom });
+           // map.Widgets.Add(new Mapsui.Widgets.Zoom.ZoomInOutWidget());
+            mapView.Map = map;
+
+        }
+
+        void AddEvent_Clicked(object sender, System.EventArgs e)
+        {
+            testCounter++;
+            var pin = new Mapsui.UI.Forms.Pin(mapView)
+            {
+                Label = testCounter.ToString(),
+                Address = e.ToString(),
+                Type = Mapsui.UI.Forms.PinType.Pin
+
+            };
+        }
+    }
+}
